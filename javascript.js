6f20953dcb1fc06fcb9527e4ab69b1bece8e2290
Cherise <cherise23.1@gmail.com>
2018-06-29 16:01:02 +0000

    adding new files.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,106 @@
+// Okay to Delete!
+/* Learning Arrays
 console.log("something");
+
+var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "SuperDay!"];
+
+console.log(days [0, 2]); //arrays used to hold similar items
+
+//days.length //length shows you how many items in array
+
+var length = days.length;
+
+//console.log(days.length);
+
+var counter = 0;
+
+while ( counter < length ) {
+    
+    //console.log([0]); // to call Monday
+    //console.log("Hello!");
+    console.log(days[counter]);
+    
+    counter = counter + 1;
+}
+
+
+var favoritefruits = ["Kiwi", "Apple", "Banana", "Dragonfruit", "Strawberry", "Blueberries"];
+
+console.log("My favorite fruits are:");
+
+counter2 = 0;
+
+while (counter < 10) {
+    
+}
+   //create counter    compare counter   do something w/ counter
+for (var counter3 = 0;  counter3 < 10;   counter3 + 1) {
+    
+    
+    
+} 
+
+
+//Learning about Objects instead of arrays
+var exampleArray = [1,2,3,4];
+
+var exampleObject = { "firstNumbert": 1, "secondNumber": 2, "thirdNumber": 3};
+
+console.log(exampleObject["secondNumber"]); //object can use index address
+
+console.log(exampleObject.thirdNumber); // how to access objects
+*/
+var user = {
+    
+    "username": "eagarcia", //use comma after object
+    "password": "123456",
+    "accountId": "0000001",
+    "isActive": true
+};
+/*
+//console.log("The User name is: " + user.username);
+console.log("The password is: " + user.password);
+
+var honda = {
+    "car-serial": 1234,
+    "owner": "Cherise cars"
+};
+
+var toyota = {
+    "car-serial": 1234,
+    "owner": "Cherise"
+};
+
+
+var cars = [honda, toyota];
+
+console.log("The serial is: " + cars[0]["car-serial"]);
+
+console.log("The owner is: " + cars[1]["owner"]);
+
+var cheriseInfo = {
+    
+    "favcolor": 
+}
+*/
+
+//Learning Functions
+//Some Functions we have used
+//Math.random();
+//Math.round();
+//console.log();
+
+//var myObject = {};
+
+myFirstFunction(); //runs the 3 console.log
+
+function myFirstFunction() {
+    
+    console.log("hi");
+    
+    console.log("my name is");
+    
+    console.log("Cherise");
+    
+} //no semicolon for functions!
+
